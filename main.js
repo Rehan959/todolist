92d0a5e679057afbdbcf9b81d49a8c9aef0cd0e9
Rehan959 <rehanshami007@gmail.com>
2020-11-17 16:27:09 -0500

    todolist

---
+++ main.js
@@ -0,0 +1,23 @@
+const text = document.querySelector("#text");
+const submit = document.querySelector("#submit");
+var todoitems = [];
+const ulElm = document.querySelector("ul");
+submit.addEventListener("click", addToList);
+function addToList() {
+  if (text.value === "") {
+    alert("plese enter a todo something in todolist");
+}
+else{    
+  const liElm = document.createElement("li");
+  liElm.innerHTML = text.value;
+  ulElm.appendChild(liElm);
+  todoitems.push(text.value);
+   localStorage.setItem("todoitems",JSON.stringify(todoitems));
+  text.value = "";
+}
+}
+const getTodo = () => {
+  const data =  localStorage.getItem("todoitems");
+  todoitems = JSON.parse(data);
+}
+getTodo();
