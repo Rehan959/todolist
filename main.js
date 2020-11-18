06cd1be800b5dd866f5a109f7a1cf3f01ceba1e4
Rehan959 <rehanshami007@gmail.com>
2020-11-17 20:29:44 -0500

    todolist

--- main.js
+++ main.js
@@ -6,23 +6,22 @@ submit.addEventListener("click", addToList);
 function addToList() {
   if (text.value === "") {
     alert("plese enter a todo something in todolist");
-}
-else{    
-  const liElm = document.createElement("li");
-  liElm.innerHTML = text.value;
-  ulElm.appendChild(liElm);
-  todoitems.push(text.value);
-   localStorage.setItem("todoitems",JSON.stringify(todoitems));
-  text.value = "";
-}
+  } else {
+    const liElm = document.createElement("li");
+    liElm.innerHTML = text.value;
+    ulElm.appendChild(liElm);
+    todoitems.push(text.value);
+    localStorage.setItem("todoitems", JSON.stringify(todoitems));
+    text.value = "";
+  }
 }
 const getTodo = () => {
-  const data =  localStorage.getItem("todoitems");
+  const data = localStorage.getItem("todoitems");
   todoitems = JSON.parse(data);
-  todoitems.forEach(element => {
+  todoitems.forEach((element) => {
     const liElm = document.createElement("li");
-  liElm.innerHTML = element;
-  ulElm.appendChild(liElm);
+    liElm.innerHTML = element;
+    ulElm.appendChild(liElm);
   });
-}
+};
 getTodo();
